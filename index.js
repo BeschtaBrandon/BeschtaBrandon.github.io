ddd9d0f5a64a762fb3b78d4233ba9311be9554e9
BeschtaBrandon <blbeschta@wisc.edu>
2016-09-01 00:03:55 -0500

    Modified download button

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-$(window).load(function() {
-    //show();
-});
-
-
-function show() {
-    $('#container').hide();
-    $('#progress').fadeIn();
-};
-
-setTimeout(show, 3000);
+function success() {
+    document.getElementById("downloadBtn").innerHTML = "Success!";
+  };
