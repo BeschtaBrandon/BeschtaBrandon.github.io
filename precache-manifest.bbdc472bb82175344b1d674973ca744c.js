a021011b50d2dcd0c92fc18c4d385fa1e41ce58e
BeschtaBrandon <blbeschta@wisc.edu>
2018-12-29 00:35:45 -0600

    Updates

---
+++ precache-manifest.bbdc472bb82175344b1d674973ca744c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "421689fb798fafa97d92c8c028936eff",
+    "url": "/static/media/portrait.421689fb.jpg"
+  },
+  {
+    "revision": "499ba8503efaac88616b2793e36a58c1",
+    "url": "/static/media/outer_space.499ba850.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22f2ff17c335ec96259d",
+    "url": "/static/js/main.22f2ff17.chunk.js"
+  },
+  {
+    "revision": "1116ed9231856634700d",
+    "url": "/static/js/1.1116ed92.chunk.js"
+  },
+  {
+    "revision": "22f2ff17c335ec96259d",
+    "url": "/static/css/main.aa041dc6.chunk.css"
+  },
+  {
+    "revision": "edd02ab2c9e3af8031856ac9a492a61f",
+    "url": "/index.html"
+  }
+];
