b274e23b5c49534a992d37ab119b798a14bb331d
Brandon Beschta <brandonbeschta@Brandons-MacBook-Pro.local>
2023-01-16 20:01:22 -0600

    Updates

---
+++ precache-manifest.1e18ac03ff120b174999a30fb1000389.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "13f325d9e962e9548c72",
+    "url": "/static/css/main.78667e63.chunk.css"
+  },
+  {
+    "revision": "13f325d9e962e9548c72",
+    "url": "/static/js/main.13f325d9.chunk.js"
+  },
+  {
+    "revision": "5d3412965be6cbd29347",
+    "url": "/static/css/1.5bd2d5b6.chunk.css"
+  },
+  {
+    "revision": "5d3412965be6cbd29347",
+    "url": "/static/js/1.5d341296.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "499ba8503efaac88616b2793e36a58c1",
+    "url": "/static/media/outer_space.499ba850.jpg"
+  },
+  {
+    "revision": "0b9e0f3cb34f384a22acab2ac3ec36a3",
+    "url": "/static/media/portrait.0b9e0f3c.jpg"
+  },
+  {
+    "revision": "eb36dd951e389258a46cb7ab54a58b47",
+    "url": "/index.html"
+  }
+];
